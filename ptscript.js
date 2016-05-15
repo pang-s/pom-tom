8318c0e7efb7b5d8c6ee7d81eb2f22e39fcec712
pang <Pang@Pangs-MBP.lan>
2016-05-15 23:52:52 +1200

    tidy up

--- ptscript.js
+++ ptscript.js
@@ -1,123 +1,115 @@
 var STARS = 0;
 function startTimer(duration, display) {
-var timer = duration, minutes, seconds;
-var power_on = true;
-setInterval(function () {
-    if (power_on){
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
+  var timer = duration, minutes, seconds;
+  var power_on = true;
 
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+  setInterval(function () {
+      if (power_on){
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
 
-    display.textContent = minutes + ":" + seconds;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    if (--timer < 0) {
-      if(STARS < 3){
-      alert('Time\'s up! A new star is shining, take a mini break.');
-    }
-    else{
-      alert('Time\'s up! You\'re a star! You deserve a long break.');
-    }
-      power_on = false;
-      STARS += 1;
-      updateStars();
-      if (STARS < 4){
-        integrateBreak();
+      display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+          if(STARS < 3){
+          alert('Time\'s up! A new star is shining, take a mini break.');
+          }
+          else{
+            alert('Time\'s up! You\'re a star! You deserve a long break.');
+          }
+          power_on = false;
+          STARS += 1;
+          updateStars();
+          if (STARS < 4){
+            integrateBreak();
+          }
+
+          if (STARS == 4){
+            STARS = 0;
+            bigBreak();
+          }
+        }
       }
-
-      if (STARS == 4){
-        STARS = 0;
-        bigBreak();
-
-      }
-
-
-      //timer = duration;
-    }
-  }
-}, 1000);
+  }, 1000);
 }
 
 function integrateBreak(){
-if(STARS < 4){
-var fiveMins = 5*60,
-  display = document.querySelector('#time');
-integrateTimer(fiveMins, display);
-}
+  if(STARS < 4){
+  var fiveMins = 5*60,
+    display = document.querySelector('#time');
+  integrateTimer(fiveMins, display);
+  }
 }
 
 function bigBreak(){
-
-var twentyFiveMins = 20*60,
-  display = document.querySelector('#time');
-integrateTimer(twentyFiveMins, display);
-
+  var twentyFiveMins = 20*60,
+    display = document.querySelector('#time');
+  integrateTimer(twentyFiveMins, display);
 }
 
 
-function integrateTimer(duration, display) {
-var timer = duration, minutes, seconds;
-var power_on = true;
-setInterval(function () {
-if (power_on){
-minutes = parseInt(timer / 60, 10);
-seconds = parseInt(timer % 60, 10);
+function integrateTimer(duration, display){
+  var timer = duration, minutes, seconds;
+  var power_on = true;
+  setInterval(function (){
+    if (power_on){
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
 
-minutes = minutes < 10 ? "0" + minutes : minutes;
-seconds = seconds < 10 ? "0" + seconds : seconds;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
 
-display.textContent = minutes + ":" + seconds;
+      display.textContent = minutes + ":" + seconds;
 
-if (--timer < 0) {
-  alert('Break\'s up!');
-  power_on = false;
-  if (STARS != 0){
-  startTiming();
-}
-else{
+      if (--timer < 0) {
+        alert('Break\'s up!');
+        power_on = false;
 
-  document.getElementById("startButton").style.visibility = "visible";
-}
-  //timer = duration;
-}
-}
-}, 1000);
+        if (STARS != 0){
+          startTiming();
+        }
+        else{
+          document.getElementById("startButton").style.visibility = "visible";
+        }
+
+      }
+    }
+  }, 1000);
 }
 
 
 function updateStars(){
-starDisplay = document.querySelector('#stars');
-if (STARS == 0){
-starDisplay.textContent = String.fromCodePoint(11088) + String.fromCodePoint(11088)
-+ String.fromCodePoint(11088) + String.fromCodePoint(11088);
-}
-if (STARS == 1){
-starDisplay.textContent = String.fromCodePoint(127775)+ String.fromCodePoint(11088)
-+ String.fromCodePoint(11088) + String.fromCodePoint(11088);
-}
-else if(STARS == 2){
-starDisplay.textContent = String.fromCodePoint(127775)+ String.fromCodePoint(127775)
-+ String.fromCodePoint(11088) + String.fromCodePoint(11088);
-}
-else if(STARS == 3){
-starDisplay.textContent = String.fromCodePoint(127775)+ String.fromCodePoint(127775)
-+ String.fromCodePoint(127775) + String.fromCodePoint(11088);
-}
-else{
-starDisplay.textContent = String.fromCodePoint(127775)+ String.fromCodePoint(127775)
-+ String.fromCodePoint(127775) + String.fromCodePoint(127775);
-
-}
+  starDisplay = document.querySelector('#stars');
+  if (STARS == 0){
+  starDisplay.textContent = String.fromCodePoint(11088) + String.fromCodePoint(11088)
+  + String.fromCodePoint(11088) + String.fromCodePoint(11088);
+  }
+  if (STARS == 1){
+  starDisplay.textContent = String.fromCodePoint(127775)+ String.fromCodePoint(11088)
+  + String.fromCodePoint(11088) + String.fromCodePoint(11088);
+  }
+  else if(STARS == 2){
+  starDisplay.textContent = String.fromCodePoint(127775)+ String.fromCodePoint(127775)
+  + String.fromCodePoint(11088) + String.fromCodePoint(11088);
+  }
+  else if(STARS == 3){
+  starDisplay.textContent = String.fromCodePoint(127775)+ String.fromCodePoint(127775)
+  + String.fromCodePoint(127775) + String.fromCodePoint(11088);
+  }
+  else{
+  starDisplay.textContent = String.fromCodePoint(127775)+ String.fromCodePoint(127775)
+  + String.fromCodePoint(127775) + String.fromCodePoint(127775);
+  }
 }
 
 
 
 function startTiming() {
-document.getElementById("startButton").style.visibility = "hidden";
-var twentyFiveMins = 25*60,
-    display = document.querySelector('#time');
-startTimer(twentyFiveMins, display);
-
-
-};
+  document.getElementById("startButton").style.visibility = "hidden";
+  var twentyFiveMins = 25*60,
+      display = document.querySelector('#time');
+  startTimer(twentyFiveMins, display);
+}
